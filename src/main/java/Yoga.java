ef8b189391e008f00362be97fc9494a0ec4f3928
Nick <nkuuee@bk.ru>
2025-05-01 21:48:50 +0300

    job4j

--- src/main/java/Yoga.java
+++ src/main/java/Yoga.java
@@ -6,18 +6,39 @@ public class Yoga {
     private final int age;
     private final String city;
     private final String[] color;
+    private final Yoga_1 yoga1;
 
-    public Yoga(boolean alive, int age, String city, String[] color) {
+    public Yoga(boolean alive, int age, String city, String[] color, Yoga_1 yoga1) {
         this.alive = alive;
         this.age = age;
         this.city = city;
         this.color = color;
+        this.yoga1 = yoga1;
+    }
+
+    @Override
+    public String toString() {
+        return "Yoga{" +
+                "alive=" + alive +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                ", color=" + java.util.Arrays.toString(color) +
+                ", yoga1=" + yoga1 +
+                '}';
     }
 
 
+    public static class Yoga_1 {
+        private final String areyouAProgrammer;
+
+        public Yoga_1(String areyouAProgrammer) {
+            this.areyouAProgrammer = areyouAProgrammer;
+        }
+    }
+
     public static void main(String[] args) {
         final Yoga json = new Yoga(true, 90, "Moscow", new String[]{
-                "blue", "red", "green"});
+                "blue", "red", "green"}, new Yoga_1("sdf"));
 
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(json));
@@ -33,6 +54,8 @@ public class Yoga {
         System.out.println(personMod);
     }
 }
+
+
 
 
 
