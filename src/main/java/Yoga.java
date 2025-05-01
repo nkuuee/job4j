209932fc1d5846e2f9ffb5af2ae097b22b4b99e6
Nick <nkuuee@bk.ru>
2025-05-01 22:54:24 +0300

    job4j

--- src/main/java/Yoga.java
+++ src/main/java/Yoga.java
@@ -1,12 +1,29 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+import jakarta.xml.bind.annotation.*;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+@XmlRootElement(name = "Yoga")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Yoga {
-    private final boolean alive;
-    private final int age;
-    private final String city;
-    private final String[] color;
-    private final Yoga_1 yoga1;
+
+    @XmlAttribute
+    private boolean alive;
+    private int age;
+    private String city;
+    private String[] color;
+    private Yoga_1 yoga1;
+
+    public Yoga() {
+    }
 
     public Yoga(boolean alive, int age, String city, String[] color, Yoga_1 yoga1) {
         this.alive = alive;
@@ -28,18 +45,50 @@ public class Yoga {
     }
 
 
+    @XmlRootElement(name = "Yoga_1")
     public static class Yoga_1 {
-        private final String areyouAProgrammer;
+
+        @XmlAttribute
+        private String areyouAProgrammer;
+
+        public Yoga_1() {
+        }
 
         public Yoga_1(String areyouAProgrammer) {
             this.areyouAProgrammer = areyouAProgrammer;
         }
+
+        @Override
+        public String toString() {
+            return "Yoga_1{" +
+                    "areyouAProgrammer='" + areyouAProgrammer + '\'' +
+                    '}';
+        }
     }
 
-    public static void main(String[] args) {
-        final Yoga json = new Yoga(true, 90, "Moscow", new String[]{
+    public static void main(String[] args) throws Exception {
+        final Yoga yoga = new Yoga(true, 90, "Moscow", new String[]{
                 "blue", "red", "green"}, new Yoga_1("sdf"));
 
+        JAXBContext context = JAXBContext.newInstance(Yoga.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(yoga, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+
+            Yoga result = (Yoga) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+
+        /*
         final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(json));
 
@@ -52,6 +101,8 @@ public class Yoga {
 
         final Yoga personMod = gson.fromJson(yogaJSON, Yoga.class);
         System.out.println(personMod);
+
+         */
     }
 }
 
