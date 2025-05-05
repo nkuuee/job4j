8a75b8229d3185193c44861d8035eda4659d555b
nkuuee <nkuuee@Bk.ru>
2025-05-05 22:22:01 +0300

    Полный говнокод

--- src/main/java/Theme_6/Easy.java
+++ src/main/java/Theme_6/Easy.java
@@ -1,49 +1,22 @@
 package Theme_6;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.*;
-
-import static java.util.Arrays.asList;
+import java.util.Scanner;
 
 public class Easy {
 
-
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        String res = sc.nextLine();
-        String[] result = res.split(" ");
+        Scanner scanner = new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+        String[] numbers = inputLine.split(" ");
 
-
-        for (int j = result.length - 1; j >= 0; j--) {
-            //  String smcxm = result[j];
-            int sdfjlsdfl = Integer.parseInt(result[j]);
-            if (sdfjlsdfl % 2 == 0) {
-                System.out.print(sdfjlsdfl + " ");
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            int number = Integer.parseInt(numbers[i]);
+            if (number % 2 == 0) {
+                System.out.print(number + " ");
             }
-
-
         }
 
-
-
-
-/*
-                if (sdfasdf % 2 == 0) {
-в
-                    List<Integer> dsff = new ArrayList<>(sdfasdf);
-
-
-                }
-
- */
-
-
-        sc.close();
+        scanner.close();
     }
-
-
 }
-
-
