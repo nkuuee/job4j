7e0e41bc39cea6e19e08f4471407483983afe6cd
nkuuee <nkuuee@Bk.ru>
2025-05-05 14:10:21 +0300

    Полны

---
+++ src/main/java/Theme_6/Easy.java
@@ -0,0 +1,49 @@
+package Theme_6;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+import static java.util.Arrays.asList;
+
+public class Easy {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String res = sc.nextLine();
+        String[] result = res.split(" ");
+
+
+        for (int j = result.length - 1; j >= 0; j--) {
+            //  String smcxm = result[j];
+            int sdfjlsdfl = Integer.parseInt(result[j]);
+            if (sdfjlsdfl % 2 == 0) {
+                System.out.print(sdfjlsdfl + " ");
+            }
+
+
+        }
+
+
+
+
+/*
+                if (sdfasdf % 2 == 0) {
+
+                    List<Integer> dsff = new ArrayList<>(sdfasdf);
+
+
+                }
+
+ */
+
+
+        sc.close();
+    }
+
+
+}
+
+
