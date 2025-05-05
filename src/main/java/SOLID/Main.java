b4ad812ae6ea50dc61c1739362b103c77b6566c1
nkuuee <nkuuee@Bk.ru>
2025-05-06 00:42:58 +0300

    см. класc Main

--- src/main/java/SOLID/Main.java
+++ src/main/java/SOLID/Main.java
@@ -3,6 +3,7 @@ package SOLID;
 public class Main {
     public static void main(String[] args) {
         //Реализовать вывод.
+        //Возможно, он не нужен и можно реализовать только юнит тест.
     }
 }
 
